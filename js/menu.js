aaa8fc886bca48eb69d5ce2c3276f670e6497b3b
AZARIA <allysaidi64@gmail.com>
2021-09-06 19:45:37 +0200

    fix eslint, stylelint, webhint errors

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,26 @@
-const menu=document.querySelector("#sandwich");
-const crossElement=document.querySelector("#bar img");
-const mobileMenu=document.querySelector("#mobile-menu");
-//const menuItems= document.querySelector("options");
+const menu = document.querySelector('#sandwich');
+const crossElement = document.querySelector('#bar img');
+const mobileMenu = document.querySelector('#mobile-menu');
+// const menuItems= document.querySelector("options");
 
-const displayMenu=()=>{
-    
-    menu.addEventListener("click",function(event){
-        document.querySelector("#mobile-menu").classList.remove("hidden");
-    })
-}
-const hideMenu=()=>{
-    crossElement.addEventListener("click",function(){
-        mobileMenu.classList.add("hidden")
+const displayMenu = () => {
+  menu.addEventListener('click', () => {
+    document.querySelector('#mobile-menu').classList.remove('hidden');
+  });
+};
+const hideMenu = () => {
+  crossElement.addEventListener('click', () => {
+    mobileMenu.classList.add('hidden');
+  });
+};
+const menuItemsClicked = () => {
+  const anchors = document.querySelectorAll('.options li a');
+  anchors.forEach((anchor) => {
+    anchor.addEventListener('click', () => {
+      mobileMenu.classList.add('hidden');
     });
-}
-const menuItemsClicked=()=>{
-    let anchors=document.querySelectorAll(".options li a");
-    console.log(anchors);
-    anchors.forEach((anchor)=>{
-        anchor.addEventListener("click",function(e){
-            mobileMenu.classList.add("hidden")
-        });
-    })
-}
+  });
+};
 hideMenu();
 displayMenu();
 menuItemsClicked();
