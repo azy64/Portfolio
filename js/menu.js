557ba89ca1a58d48cab6d0feb7a58f550363dd01
AZARIA <allysaidi64@gmail.com>
2021-09-06 19:00:00 +0200

    add javascript code for the menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,32 @@
+const menu=document.querySelector("#sandwich");
+const crossElement=document.querySelector("#bar img");
+const mobileMenu=document.querySelector("#mobile-menu");
+//const menuItems= document.querySelector("options");
+
 const displayMenu=()=>{
-    //console.log("sarting the operation...");
-    let menu=document.querySelector(".mobile-menu");
+    
     menu.addEventListener("click",function(event){
-        document.querySelector(".tunaweza-menu").classList.remove("hidden");
-        event.stopPropagation();
-        //console.log(event);
-    },true)
+        document.querySelector("#mobile-menu").classList.remove("hidden");
+    })
 }
 const hideMenu=()=>{
-
+    crossElement.addEventListener("click",function(){
+        mobileMenu.classList.add("hidden")
+        console.log(document.querySelector(".tunaweza-menu").classList);
+        console.log(mobileMenu.className);
+        //e.preventDefault();
+    });
 }
+const menuItemsClicked=()=>{
+    let anchors=document.querySelectorAll(".options li a");
+    console.log(anchors);
+    anchors.forEach((anchor)=>{
+        anchor.addEventListener("click",function(e){
+            mobileMenu.classList.add("hidden")
+            //e.preventDefault();
+        });
+    })
+}
+hideMenu();
 displayMenu();
+menuItemsClicked();
